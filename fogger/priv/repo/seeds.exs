54837a503f51832b7e39f85d0cc1218c3e3847f5
Dev <devika@hiivemarkets.com>
2024-01-31 18:28:29 -0500

    fix: added next and prev queries
    - added seed data for MovieQuotes
    
    ref: NO-TICKET

--- fogger/priv/repo/seeds.exs
+++ fogger/priv/repo/seeds.exs
@@ -11,9 +11,16 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Fogger.Accounts
+alias Fogger.Library
 # alias Fogger.Repo
 
-Accounts.register_user(%{email: "yoda1@may4th.com", password: "lukeskywalker"})
-Accounts.register_user(%{email: "yoda2@may4th.com", password: "lukeskywalker"})
+#Accounts.register_user(%{email: "yoda1@may4th.com", password: "lukeskywalker"})
+#Accounts.register_user(%{email: "yoda2@may4th.com", password: "lukeskywalker"})
 Accounts.register_user(%{email: "yoda3@may4th.com", password: "lukeskywalker"})
-Accounts.register_user(%{email: "yoda4@may4th.com", password: "lukeskywalker"})
+#Accounts.register_user(%{email: "yoda4@may4th.com", password: "lukeskywalker"})
+
+Library.create_movie_quote(%{text: "do or do not, there is no try", steps: 2, name: "yoda"})
+Library.create_movie_quote(%{text: "So much space for activities", steps: 3, name: "brennan"})
+Library.create_movie_quote(%{text: "They’re taking the Hobbits to Isengard", steps: 2, name: "legolas"})
+Library.create_movie_quote(%{text: "I trust him as far as I can throw him", steps: 4, name: "unknown"})
+Library.create_movie_quote(%{text: "I'll be back", steps: 2, name: "terminator"})
